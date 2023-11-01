ea9d32754a3ca28987abfee44397a1daa48dea11
justinl102 <labrash.justin@gmail.com>
2023-11-01 14:47:14 -0400

    add caching

--- main.py
+++ main.py
@@ -16,8 +16,11 @@ def main():
 if __name__ == '__main__':
     main()
 
+@st.cache_data
+def load_dataset(path):
+    return pd.read_pickle("growth_df.pkl")
 
-df = pd.read_pickle("growth_df.pkl")
+df = load_dataset("growth_df.pkl")
 df['monitoring_date'] = pd.to_datetime(df['monitoring_date'])
 df['harvest_date'] = pd.to_datetime(df['harvest_date'])
 df['stockedAt'] = pd.to_datetime(df['stockedAt'])
